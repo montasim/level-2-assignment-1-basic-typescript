312d5bd52876022edcfa471a45c0209b14167ca3
MONTASIM <mmamun@mymedicalhub.com>
2023-05-09 20:14:34 +0600

    optional problem 1

---
+++ src/optional-problem-1.ts
@@ -0,0 +1,11 @@
+const arrayOfString2: string[] = ['Rahim', 'Karim', 'Abul', 'Kabul', 'Babul'];
+let tupleOfNumberAndString: [age: number, name: string] = [20, 'Rahim Mia'];
+
+console.log(arrayOfString2);
+console.log(tupleOfNumberAndString);
+
+// arrayOfString2.push('Kamal');
+tupleOfNumberAndString = [18, 'Monju'];
+
+console.log(arrayOfString2);
+console.log(tupleOfNumberAndString);
