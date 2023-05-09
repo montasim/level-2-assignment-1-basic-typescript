8f13b2b46ed677269049307970abc4f5b5acfba3
MONTASIM <mmamun@mymedicalhub.com>
2023-05-09 20:34:55 +0600

    optional problem 2

---
+++ src/optional-problem-2.ts
@@ -0,0 +1,16 @@
+// part 1
+type AlphaNumericType<T> = T extends string ? string : number;
+
+// part 2
+interface PersonInterface{
+    name: string;
+    age: AlphaNumericType<any>;
+}
+
+const man: PersonInterface = {
+    name: "Rahim",
+    age: 20
+}
+
+// part 3
+console.log(man);
