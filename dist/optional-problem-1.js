2570c1a424e3bc69a822c96d3a5f4e8cd39808c4
MONTASIM <mmamun@mymedicalhub.com>
2023-05-09 20:35:51 +0600

    corresponding js code generated

---
+++ dist/optional-problem-1.js
@@ -0,0 +1,9 @@
+"use strict";
+const arrayOfString2 = ['Rahim', 'Karim', 'Abul', 'Kabul', 'Babul'];
+let tupleOfNumberAndString = [20, 'Rahim Mia'];
+console.log(arrayOfString2);
+console.log(tupleOfNumberAndString);
+// arrayOfString2.push('Kamal');
+tupleOfNumberAndString = [18, 'Monju'];
+console.log(arrayOfString2);
+console.log(tupleOfNumberAndString);
---
+++ dist/optional-problem-2.js
@@ -0,0 +1,7 @@
+"use strict";
+const man = {
+    name: "Rahim",
+    age: 20
+};
+// part 3
+console.log(man);
