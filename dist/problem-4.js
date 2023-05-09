f18b5dd7f892f8dd93b0408b7578d82bd6905c87
MONTASIM <mmamun@mymedicalhub.com>
2023-05-09 19:32:25 +0600

    problem 4

--- dist/problem-4.js
+++ dist/problem-4.js
@@ -1,1 +1,25 @@
 "use strict";
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    getDetails() {
+        return console.log(`${this.name} ${this.age}`);
+    }
+}
+class Student extends Person {
+    constructor(name, age, grade) {
+        super(name, age);
+        this.grade = grade;
+    }
+    getGrade() {
+        return console.log(this.grade);
+    }
+}
+const person1 = new Person('Rahim Mia', 20);
+const student1 = new Student('Karim Mia', 18, 'A+');
+person1.getDetails();
+student1.getGrade();
+console.log(person1);
+console.log(student1);
---
+++ src/problem-4.ts
@@ -0,0 +1,32 @@
+class Person{
+    private name: string;
+    private age: number; 
+
+    constructor(name: string, age: number){
+        this.name = name;
+        this.age = age;
+    }
+
+    public getDetails(): void{
+        return console.log(`${this.name} ${this.age}`);
+    }
+}
+
+class Student extends Person{
+    private grade: string
+
+    constructor(name: string, age: number, grade: string){
+        super(name, age);
+        this.grade = grade;
+    }
+
+    public getGrade(): void{
+        return console.log(this.grade);
+    }
+}
+
+const person1 = new Person('Rahim Mia', 20);
+const student1 = new Student('Karim Mia', 18, 'A+');
+
+person1.getDetails();
+student1.getGrade();
